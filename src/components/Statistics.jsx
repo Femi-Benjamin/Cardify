31470c72606a6111bf457396d5ef3132a941a756
Olufemi Benjamin <benjaminolufemi16@gmail.com>
2024-01-22 16:05:28 -0600

    update6

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,8 +1,8 @@
-import Jumia_logo from "../assets/Jumia_logo.svg";
-import Spotify_logo from "../assets/Spotify_logo.svg";
-import Playstation_logo from "../assets/Playstation_logo.svg";
-import Deezer_logo from "../assets/Deezer_logo.svg";
-import Apple_logo from "../assets/Apple_logo.svg";
+import Jumia from "../assets/Jumia.svg";
+import Spotify from "../assets/Spotify.svg";
+import Playstation from "../assets/Playstation.svg";
+import Deezer from "../assets/Deezer.svg";
+import Apple from "../assets/Apple.svg";
 
 const websites = () => {
   return (
@@ -19,27 +19,27 @@ const websites = () => {
 
       <div className="flex flex-wrap justify-center pb-6 gap-4 md:gap-8 items-center">
         <a href="https://www.jumia.com.ng/" target="_blank" rel="noopener noreferrer">
-          <img src={Jumia_logo} alt="" className="w-16 h-16 md:w-28  md:h-20" />{" "}
+          <img src={Jumia} alt="" className="w-16 h-16 md:w-28  md:h-20" />{" "}
         </a>
         <a href="https://www.spotify.com" target="_blank" rel="noopener noreferrer">
           <img
-            src={Spotify_logo}
+            src={Spotify}
             alt=""
             className="w-16 h-16 md:w-28 md:h-20"
           />{" "}
         </a>
         <a href="https://www.playstation.com/es-ar/" target="_blank" rel="noopener noreferrer">
           <img
-            src={Playstation_logo}
+            src={Playstation}
             alt=""
             className="w-16 h-16 md:w-28 md:h-20"
           />{" "}
         </a>
         <a href="https://www.deezer.com/en/offers" target="_blank" rel="noopener noreferrer">
-          <img src={Deezer_logo} alt="" className="w-16 h-16 md:w-28 md:h-20" />{" "}
+          <img src={Deezer} alt="" className="w-16 h-16 md:w-28 md:h-20" />{" "}
         </a>
         <a href="https://www.apple.com/itunes/" target="_blank" rel="noopener noreferrer">
-          <img src={Apple_logo} alt="" className="w-16 h-16 md:w-28 md:h-20" />
+          <img src={Apple} alt="" className="w-16 h-16 md:w-28 md:h-20" />
         </a>
       </div>
     </div>
